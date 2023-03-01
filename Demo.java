a8af335b4ad112b7d610d469f2246a83b0d401b4
gsainicse <gsaini.cse@gmail.com>
2023-03-01 18:43:48 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Calculator{
+    public int sum(int a, int b){
+    return a+b;
+    }
+}
+public class Demo{
+    public static void main(String[] args) {
+        int num1=1;
+        int num2=3;
+        Calculator cal= new Calculator();
+        System.out.println(cal.sum(num1,num2));
+    }
+}
