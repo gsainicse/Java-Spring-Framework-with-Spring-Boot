2184530da53d19f2f2817b03a95cc6e52f39d891
gsainicse <gsaini.cse@gmail.com>
2023-03-07 15:47:36 +0530

    Excception

---
+++ Catch.java
@@ -0,0 +1,10 @@
+public class Catch {
+    public static void main(String args[]){
+        try{
+            int b=1/1;
+        }
+        catch(Exception e){
+            System.out.println("oh we have exceptions");
+        }
+    }
+}
