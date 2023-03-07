5581e2a8d0505baa1466e2ad08fdf53fec5bf438
gsainicse <gsaini.cse@gmail.com>
2023-03-07 20:20:59 +0530

    Scanner Class

---
+++ ScannerClass.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ScannerClass {
+        public static void main(String args[]){
+            Scanner in = new Scanner(System.in);
+            int num=in.nextInt();
+            System.out.println(num);
+            in.close();
+        }    
+}
